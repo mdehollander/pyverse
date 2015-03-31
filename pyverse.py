be47672622dc66407bf7028337b0fd8bd4fc7a28
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-03-31 22:32:24 +0200

    Added Datafile class

--- pyverse.py
+++ pyverse.py
@@ -54,10 +54,32 @@ class Dataset(object):
         if apikey:
             url += "?key={apikey}".format(apikey=config.api_token)
 
+        print(url)
         return url
 
     def view_info(self):
         return requests.get(self.get_url("", apikey=True)).json()
 
-    def view_files(self):
-        return requests.get(self.get_url("/files", apikey=True)).json()
+    def view_files(self, version_id=None):
+        if version_id is None:
+            version_id = ":latest"
+        return requests.get(self.get_url("/versions/{version}/files".format(version=version_id), apikey=True)).json()
+
+    def get_datafiles(self):
+        dfs = []
+        for df in self.view_files()['data']:
+            dfs.append(Datafile(df['datafile']['id'], df['datafile']['name']))
+        return dfs
+
+
+class Datafile(object):
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def download(self):
+        url = "https://{server}/api/access/datafile/{id}?format=original".format(server=config.server,
+                                                                 id=self.id)
+        with open(self.name, "wb") as f:
+            f.write(requests.get(url).content)
